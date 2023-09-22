8f335b489168d11c8194ed98f512f75cd9a661fd
orla-ske <kodjoorlando@yahoo.co.uk>
2023-09-22 13:50:06 +0000

    added doctor entity

---
+++ src/main/java/com/example/demo/entity/Doctor.java
@@ -0,0 +1,30 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+
+public class Doctor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String name;
+
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    } 
+
+}
